283d48c51f5606409881b814d1e6897e2a05a48e
Esteban Hernandez <esteban.hernandez@nexted.com.au>
2024-05-09 18:05:47 +1000

    Drawing Book java solution

---
+++ Algorithms/Implementation/Drawing Book/Solution.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'pageCount' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER p
+     */
+
+    public static int pageCount(int n, int p) {
+    // Write your code here
+        int minPages = Integer.MAX_VALUE;
+        int backCover = Integer.MAX_VALUE;;
+        if(p == 1 || p == n) return 0;
+        
+        //preference to start from the beginning
+        minPages = p/2;
+        
+        //calculate from back cover
+        backCover = Math.abs((n/2 - p/2));
+        if(backCover < minPages) return backCover;
+                
+        return minPages;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int p = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.pageCount(n, p);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
