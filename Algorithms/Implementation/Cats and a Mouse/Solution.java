039612d407474c54307e7229b32b92e4e3c9db7a
Esteban Hernandez <esteban.hernandez@nexted.com.au>
2024-05-09 18:04:56 +1000

    Cats and a Mouse java solution

---
+++ Algorithms/Implementation/Cats and a Mouse/Solution.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the catAndMouse function below.
+    static String catAndMouse(int x, int y, int z) {
+        int difCatA = Integer.MAX_VALUE;
+        int difCatB = Integer.MAX_VALUE;
+        difCatA = Math.abs(x-z);
+        difCatB = Math.abs(y-z);
+        if(difCatA == difCatB) return "Mouse C";
+        
+        //The bigger the difference, the further the cat is
+        if(difCatA > difCatB) return "Cat B";
+        else{
+            return "Cat A";
+        }
+        
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String[] xyz = scanner.nextLine().split(" ");
+
+            int x = Integer.parseInt(xyz[0]);
+
+            int y = Integer.parseInt(xyz[1]);
+
+            int z = Integer.parseInt(xyz[2]);
+
+            String result = catAndMouse(x, y, z);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
